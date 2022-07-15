58d592e133749b835baab62b75f9a2690570d708
Ramiz Bozai <ramiz.bozai@fishtownanalytics.com>
2022-07-15 19:27:00 +0000

    Added new examples

--- snapshots/my_first_snapshot.sql
+++ snapshots/my_first_snapshot.sql
@@ -1,16 +1,14 @@
 {% snapshot my_first_snapshot %}
+    {{
+        config(
+            target_database=generate_database_name(),
+            target_schema='snapshots',
+            unique_key='id',
+            strategy='timestamp',
+            updated_at='_etl_loaded_at'
 
-{{
-    config(
-      target_database='ramiz_bozai_sandbox',
-      target_schema='snapshots',
-      unique_key='id',
+        )
+    }}
 
-      strategy='timestamp',
-      updated_at='_etl_loaded_at',
-    )
-}}
-
-select * from {{ source('jaffle_shop', 'orders') }}
-
-{% endsnapshot %}
+    select * from {{ source('jaffle_shop', 'orders') }}
+ {% endsnapshot %}
