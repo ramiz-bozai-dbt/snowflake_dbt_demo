856220ee342b2e8262ac0355f2637b2958e2ca21
Ramiz Bozai <ramiz.bozai@fishtownanalytics.com>
2022-09-08 18:00:28 +0000

    Remove commented line

--- macros/get_custom_schema.sql
+++ macros/get_custom_schema.sql
@@ -1,5 +1,3 @@
--- put this in macros/get_custom_schema.sql
-
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {{ generate_schema_name_for_env(custom_schema_name, node) }}
 {%- endmacro %}
