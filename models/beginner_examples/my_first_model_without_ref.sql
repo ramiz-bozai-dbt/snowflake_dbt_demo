c4da606645affffc36ef4831c047d23ef689ddff
Ramiz Bozai <ramiz.bozai@fishtownanalytics.com>
2022-11-01 15:38:36 +0000

    Add ignore target back

--- models/beginner_examples/my_first_model_without_ref.sql
+++ models/beginner_examples/my_first_model_without_ref.sql
@@ -5,3 +5,4 @@ select
     completions/attempts as completion_pct --Let's calculate the completion rate.
 
 from ramiz_bozai_sandbox_dev.dbt_rbozai.stg_passing_stats --Not portable!
+--Add a comment
