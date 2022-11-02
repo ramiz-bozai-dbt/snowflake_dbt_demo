40587da403ba7b9e2eccb56796d99dc1a3901b6f
ramiz-bozai-dbt <107953458+ramiz-bozai-dbt@users.noreply.github.com>
2022-11-02 11:03:37 -0400

    Delete target directory

--- target/run/ramiz_snowflake_github_demo/models/beginner_examples/materialization_incremental.sql
+++
@@ -1,9 +0,0 @@
-begin;
-    
-
-        insert into ramiz_bozai_sandbox_dev.dbt_rbozai.materialization_incremental ("CUSTOMER_KEY", "NAME", "ADDRESS", "NATION_KEY", "PHONE_NUMBER", "ACCOUNT_BALANCE", "MARKET_SEGMENT", "COMMENT")
-        (
-            select "CUSTOMER_KEY", "NAME", "ADDRESS", "NATION_KEY", "PHONE_NUMBER", "ACCOUNT_BALANCE", "MARKET_SEGMENT", "COMMENT"
-            from ramiz_bozai_sandbox_dev.dbt_rbozai.materialization_incremental__dbt_tmp
-        );
-    commit;
