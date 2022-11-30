11c9b0d0dae238d169ef66255cb346f8eebda015
Ramiz Bozai <ramiz.bozai@fishtownanalytics.com>
2022-11-30 15:14:41 +0000

    Removed cols

--- models/beginner_examples/my_first_model.sql
+++ models/beginner_examples/my_first_model.sql
@@ -2,9 +2,7 @@
 
 select
     *,
-    completions/attempts as completion_pct,
-    1 as sample_col,
-    1 as sample_cols  --Let's calculate the completion rate.
+    completions/attempts as completion_pct --Let's calculate the completion rate.
 
 from {{ ref('stg_passing_stats') }}
 --
