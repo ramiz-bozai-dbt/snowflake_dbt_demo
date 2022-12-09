0c1ae6293f9d5982ebf90229b6ec6f0191df2dac
Ramiz Bozai <ramiz.bozai@fishtownanalytics.com>
2022-12-09 01:05:15 +0000

    Edited generate schema name file

---
+++ macros/generate_schema_name_for_env.sql
@@ -0,0 +1,25 @@
+{% macro generate_schema_name_for_env(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    
+    {%- if target.name == 'prod' and custom_schema_name is not none -%}
+
+        {{ custom_schema_name | trim }}
+
+    
+    {%- elif target.name == 'dev' -%}
+
+        {{ default_schema}}        
+
+    
+    {%- elif target.name == 'slim_ci' -%}
+
+        {{ slim_ci_temp }}
+
+    {%- else -%}
+
+        {{ default_schema }}
+
+    {%- endif -%}
+
+{%- endmacro %}
