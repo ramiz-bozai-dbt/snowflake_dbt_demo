5a2c80dcf6502a56b22bcb29633629ac0f4820ec
Ramiz Bozai <ramiz.bozai@fishtownanalytics.com>
2022-12-21 14:57:41 +0000

    Add a column

--- models/beginner_examples/my_first_model.sql
+++ models/beginner_examples/my_first_model.sql
@@ -2,7 +2,8 @@
 
 select
     *,
-    completions/attempts as completion_pct --Let's calculate the completion rate.
+    completions/attempts as completion_pct,
+    1 as test --Let's calculate the completion rate.
 --
 from {{ ref('stg_passing_stats') }}
 
