a7683c0c70ffe15651708c39ba82002f2a7a1270
Ramiz Bozai <ramiz.bozai@fishtownanalytics.com>
2023-08-07 19:20:50 +0000

    remove a bunch of tpch and old files, upgrade packages for 1.6

--- analyses/calculate_age_completion_rate.sql
+++
@@ -1,6 +0,0 @@
-select * 
-from {{ metrics.calculate(
-    metric('avg_completion_rate_by_age_group'),
-    grain='year',
-    dimensions=['age_bucket']
-) }}
--- analyses/calculate_completion_rate_sample.sql
+++
@@ -1,6 +0,0 @@
-select *
-from {{ metrics.calculate(
-		metric('avg_completion_rate'),
-		grain='year',
-) }}
---
