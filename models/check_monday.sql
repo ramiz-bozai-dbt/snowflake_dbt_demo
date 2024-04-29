62320c51a69ece70aaf61a6647a44bb6793a36da
Ramiz Bozai <ramiz.bozai@fishtownanalytics.com>
2024-04-29 17:17:54 +0000

    monday macros

---
+++ models/check_monday.sql
@@ -0,0 +1,7 @@
+{% if execute %}
+  {% set is_monday = is_it_monday() %}
+  {% if not is_monday %}
+    {{ log("Today is not Monday. Skipping the rest of the job.", info=True) }}
+    {% do raise_compiler_error("Halting job because it's not Monday.") %}
+  {% endif %}
+{% endif %}
