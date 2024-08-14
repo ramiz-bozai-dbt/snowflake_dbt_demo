29d406aabad283415087db11a7e98172ea99a094
Ramiz Bozai <ramiz.bozai@fishtownanalytics.com>
2024-08-14 14:42:32 +0000

    removed 2 columns

--- models/beginner_examples/my_first_model.sql
+++ models/beginner_examples/my_first_model.sql
@@ -3,10 +3,7 @@
 
 select
     *,
-    1 as test,
-   --
-    --Let's calculate the completion rate.
-    completions / attempts as completion_pct,
+   
     case
         when age between 20 and 23 then '20-23'
         when age between 24 and 28 then '24-28'
