a70d7329709c35a8587f26d59056eee0d380b019
Ramiz Bozai <ramiz.bozai@fishtownanalytics.com>
2024-10-10 18:19:38 +0000

    changed to timestamp

--- models/staging/nfl/stg_passing_stats.sql
+++ models/staging/nfl/stg_passing_stats.sql
@@ -13,8 +13,10 @@ renamed as (
         age,
         pos as position,
         cmp as completions,
+        att as attempts,
+        yds as yards,
         td,
-        to_date(year, 'YYYY') as season_year,
+        to_timestamp(year, 'YYYY') as season_year,
         regexp_replace(player, '[^a-zA-Z0-9 ]', '') as player_name
 
     from source
