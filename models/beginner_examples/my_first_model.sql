cd73277da46f18cb930c2d40e5c77c27173162e6
Ramiz Bozai <ramiz.bozai@fishtownanalytics.com>
2024-11-04 14:45:04 +0000

    fix my first model

--- models/beginner_examples/my_first_model.sql
+++ models/beginner_examples/my_first_model.sql
@@ -4,7 +4,12 @@
 
 select
     *,
-    1 as test,
-    2 as another_test
+    case
+        when age between 20 and 23 then '20-23'
+        when age between 24 and 28 then '24-28'
+        when age between 29 and 40 then '29-40'
+        when age >= 41 then '40+'
+    end
+        as age_bucket
 
 from {{ ref('stg_passing_stats') }}
