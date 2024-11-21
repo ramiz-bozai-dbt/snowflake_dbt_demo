cf5c37b99ea501af2223c1c8a8538ea82ab531e8
Ramiz Bozai <ramiz.bozai@fishtownanalytics.com>
2024-11-21 21:42:23 +0000

    removed age bucket and added team category

--- models/beginner_examples/my_first_model.sql
+++ models/beginner_examples/my_first_model.sql
@@ -4,12 +4,13 @@
 
 select
     *,
+    
     case
-        when age between 20 and 23 then '20-23'
-        when age between 24 and 28 then '24-28'
-        when age between 29 and 40 then '29-40'
-        when age >= 41 then '40+'
+        when team in ('KC', 'BUF', 'SF') then 'Contender'
+        else 'No'
     end
-        as age_bucket
+        as team_category
+
+
 
 from {{ ref('stg_passing_stats') }}
