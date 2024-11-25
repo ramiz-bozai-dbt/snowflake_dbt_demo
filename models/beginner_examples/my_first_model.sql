1951f2ceb94cd60772fe10dc9089bcf48cc5dc6a
Ramiz Bozai <ramiz.bozai@fishtownanalytics.com>
2024-11-25 18:43:39 +0000

    added a column and removed some others

--- models/beginner_examples/my_first_model.sql
+++ models/beginner_examples/my_first_model.sql
@@ -2,14 +2,8 @@
 
 select
     *,
-    1 as test,
-    case
-        when age between 20 and 23 then '20-23'
-        when age between 24 and 28 then '24-28'
-        when age between 29 and 40 then '29-40'
-        when age >= 41 then '40+'
-    end
-        as age_bucket
+    2 as another_columns
+        
 
 
 from {{ ref('stg_passing_stats') }}
