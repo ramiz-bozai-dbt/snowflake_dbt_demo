fefd0057495388dbbbb501f5156f2f859ec90d22
Ramiz Bozai <ramiz.bozai@fishtownanalytics.com>
2025-01-21 15:18:15 +0000

    added a dummy column

--- models/beginner_examples/my_first_model.sql
+++ models/beginner_examples/my_first_model.sql
@@ -3,6 +3,7 @@
 
 select
     *,
+    1 as test,
     case
         when age between 20 and 23 then '20-23'
         when age between 24 and 28 then '24-28'
@@ -10,6 +11,7 @@ select
         when age >= 41 then '40+'
     end
         as age_bucket
+    
 
 
 from {{ ref('stg_passing_stats') }}
